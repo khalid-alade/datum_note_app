6cd8fc83555b10fbded088bb383766fa2054465b
Yusuf Khalid <khalidalade2004@gmail.com>
2021-12-27 02:49:52 +0300

    Second commit

--- datum/static/js/Datum_home.js
+++ datum/static/js/Datum_home.js
@@ -4,9 +4,13 @@ var set = document.querySelector('.settings')
  
 var gallery = document.getElementById('gallery_icon')
 var fileDiv = document.querySelector('.fileChoose')
+var tool1  = document.querySelector('.tooltiptext')
+var tool2  = document.querySelector('.tooltiptext2')
 
 set.addEventListener('click', function(){
     opt.classList.toggle('slide1')
+    tool1.classList.toggle('none')
+    tool2.classList.toggle('none')
 })
 
 // gallery.addEventListener('click', function(){
